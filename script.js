825ea33f9582c84ec75ace737b2c16990a41624a
John <134107082+jksnook@users.noreply.github.com>
2023-08-26 21:05:10 -0700

    Add ability to change grid size

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const container = document.querySelector('.grid-container');
 
 // create a 16 by 16 grid of squares
-for (let i=0; i<16; i++) {
-  const row = document.createElement('div');
-  row.classList.add('grid-row');
-  for (let j=0; j<16; j++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    row.appendChild(square);
+function drawGrid(size) {
+  const oldRows = document.querySelectorAll('.grid-row');
+  oldRows.forEach(row => row.remove());
+  for (let i=0; i<size; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+    for (let j=0; j<size; j++) {
+      const square = document.createElement('div');
+      square.classList.add('grid-square');
+      row.appendChild(square);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
+
+  // add event listeners to make squares turn color when moused over
+  const newSquares = document.querySelectorAll('.grid-square');
+  newSquares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+      square.classList.add('mousedOver');
+    });
+  });
 }
 
-const squares = document.querySelectorAll('.grid-square');
-console.log(squares);
+drawGrid(16);
 
-// add event listeners to make squares turn color when moused over
-squares.forEach(square => {
-  square.addEventListener('mouseover', () => {
-    square.classList.add('mousedOver');
-  });
-});
+const button = document.querySelector('.grid-select');
+
+button.addEventListener('click', () => {
+  let size = prompt('Please enter a grid size <= 100');
+  if (size > 100) size = 100;
+  drawGrid(size);
+})
